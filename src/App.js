d3248540e5c61bbaf7c9b9f34fd6defec8a439b8
Mavlon_Yusuf <mavlonjonyusupov1@gmail.com>
2024-04-21 00:21:48 +0500

    this is my first app in reactjs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello ReactJs</h1>
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export { App };
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is Navbar component</h1>
+      </header>
+    </div>
+  );
+}
+
+export { Navbar };
